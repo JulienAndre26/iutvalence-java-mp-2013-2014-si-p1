48dfa9d17a5fc705816c8c6e9884050c9ffd387a
JulienAndre26 <julien.andre@iut-valence.fr>
2013-09-20 10:02:06 +0200

    Created RollingBall class
    Start to define rules and start of the game.

---
+++ src/fr/iutvalence/java/mp/RollingBall/RollingBall.java
@@ -0,0 +1,18 @@
+package fr.iutvalence.java.mp.RollingBall;
+
+/**
+ * 
+ * Means and rules of the game.
+ *
+ */
+public class RollingBall
+{
+    /**
+     * Starting a game, by selecting a map.
+     */
+    public Map start(int numMap)
+    {
+        
+    }
+
+}
