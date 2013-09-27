6fbae0c370a7a3e58593f5406184896605fe2bfd
JulienAndre26 <julien.andre@iut-valence.fr>
2013-09-27 09:59:49 +0200

    Updated class RollingBall
    Deleted methods
    Added attributes and constructors

--- src/fr/iutvalence/java/mp/RollingBall/RollingBall.java
+++ src/fr/iutvalence/java/mp/RollingBall/RollingBall.java
@@ -3,16 +3,42 @@ package fr.iutvalence.java.mp.RollingBall;
 /**
  * 
  * Means and rules of the game.
+ * @author andrejul
  *
  */
 public class RollingBall
 {
     /**
-     * Starting a game, by selecting a map.
+     * the name of the player
      */
-    public Map start(int numMap)
+    private String a;
+    
+    /**
+     * the map
+     */
+    private Map m;
+    
+    /**
+     * the ball
+     */
+    private Ball b;
+    
+    /**
+     * the score
+     */
+    private Score s;
+    
+    /**
+     * Start a game
+     * @param a the name of the player
+     * @param m the map used by the player
+     * @param b the ball choose by the player
+     */
+    public RollingBall(String a, Map m, Ball b)
     {
-        
+        this.a = a;
+        this.m = m;
+        this.b = b;
     }
-
+    
 }
