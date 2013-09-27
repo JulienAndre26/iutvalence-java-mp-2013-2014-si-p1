ee31d4b7acb563983d088dc3c7f76738b50aaa22
JulienAndre26 <julien.andre@iut-valence.fr>
2013-09-27 09:57:59 +0200

    Created class Point
    Defined a point

---
+++ src/fr/iutvalence/java/mp/RollingBall/Point.java
@@ -0,0 +1,33 @@
+package fr.iutvalence.java.mp.RollingBall;
+
+/**
+ * 
+ * two integers which define the point
+ * @author andrejul
+ *
+ */
+public class Point
+{
+    
+    /**
+     * abscissa of the point
+     */
+    private int x;
+    
+    /**
+     * ordered of the point
+     */
+    private int y;
+    
+    /**
+     * a simple point
+     * @param x abscissa of the point
+     * @param y ordered of the point
+     */
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+}
