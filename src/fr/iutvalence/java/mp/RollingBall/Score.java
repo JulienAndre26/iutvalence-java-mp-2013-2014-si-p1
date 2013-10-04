8fed4c8c5dd553281d58d5b57bf7f6e5650c30a5
JulienAndre26 <julien.andre@iut-valence.fr>
2013-10-04 09:05:12 +0200

    Fixed TODOs

--- src/fr/iutvalence/java/mp/RollingBall/Score.java
+++ src/fr/iutvalence/java/mp/RollingBall/Score.java
@@ -1,9 +1,9 @@
 package fr.iutvalence.java.mp.RollingBall;
 
-// TODO (fix) fix comment (this class is not supposed to print something)
+// TODO (FIXED) fix comment (this class is not supposed to print something)
 /**
  * 
- * print the score of the player
+ * make the score of the player
  * @author andrejul
  *
  */
