4a8a1452291650bdb67395a2b535db278691256d
JulienAndre26 <julien.andre@iut-valence.fr>
2013-10-17 10:32:40 +0200

    Updated main.java
    add print

--- src/fr/iutvalence/java/mp/RollingBall/Main.java
+++ src/fr/iutvalence/java/mp/RollingBall/Main.java
@@ -20,28 +20,31 @@ public class Main
 
         Segment segmentWhereTheBallCanBeReleased = new Segment(beginingOfTheSegment, 
                 endingOfTheSegment, physicalOrNot);
-
+        System.out.print("segment de la balle créé\n");
+        
         boolean physicalOrNot1 = true;
         Point beginingOfTheSegment1 = new Point(0,0);
         Point endingOfTheSegment1 = new Point(100,0);
 
         Segment segment1 = new Segment(
                 beginingOfTheSegment1, endingOfTheSegment1, physicalOrNot1);
-
+        
         Segment[] segmentsOfTheField = null;
         segmentsOfTheField = new Segment[1];
         segmentsOfTheField[0] = segment1;
-
+        System.out.print("map créée\n");
+        
         Map map = new Map(segmentsOfTheField, segmentWhereTheBallCanBeReleased);
 
         Point centerOfTheBallWanted = beginingOfTheSegment;
         int radiusOfTheBallWanted = 10;
         Ball ball = new Ball(radiusOfTheBallWanted, centerOfTheBallWanted);
+        System.out.print("balle créée\n");
 
         String nameOfThePlayerPlaying = "toto";
 
         new RollingBall(nameOfThePlayerPlaying, map, ball).play();
-        System.out.print("kikou");
+        System.out.print("le jeu a commencé\n");
     }
 
 }
