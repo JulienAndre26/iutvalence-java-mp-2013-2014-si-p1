be6cd9e0bec9e2c559bf70de88f54253f62f5cf2
JulienAndre26 <julien.andre@iut-valence.fr>
2013-10-17 08:46:59 +0200

    Update Main.java
    Adapted the test to our new declaration of a segment

--- src/fr/iutvalence/java/mp/RollingBall/Main.java
+++ src/fr/iutvalence/java/mp/RollingBall/Main.java
@@ -14,36 +14,32 @@ public class Main
      */
     public static void main(String[] args)
     {
+        boolean physicalOrNot = false;
+        Point beginingOfTheSegment = new Point(50,50);
+        Point endingOfTheSegment = new Point(50,100);
 
-        int lengthOfTheSegment = 20;
-        boolean physicalOrNot = true;
-        int angleOfTheSegment = 270;
-        Point pointOfTheBeginnigOfTheSegment = new Point(50,50);
-        
-        Segment segmentWhereTheBallCanBeReleased = new Segment(pointOfTheBeginnigOfTheSegment, 
-                lengthOfTheSegment, angleOfTheSegment, physicalOrNot);
-        
-        int lengthOfTheSegment1 = 100;
+        Segment segmentWhereTheBallCanBeReleased = new Segment(beginingOfTheSegment, 
+                endingOfTheSegment, physicalOrNot);
+
         boolean physicalOrNot1 = true;
-        int angleOfTheSegment1 = 0;
-        Point pointOfTheBeginnigOfTheSegment1 = new Point(0,0);
-        
+        Point beginingOfTheSegment1 = new Point(0,0);
+        Point endingOfTheSegment1 = new Point(100,0);
+
         Segment segment1 = new Segment(
-                pointOfTheBeginnigOfTheSegment1, lengthOfTheSegment1, 
-                angleOfTheSegment1, physicalOrNot1);
-        
+                beginingOfTheSegment1, endingOfTheSegment1, physicalOrNot1);
+
         Segment[] segmentsOfTheField = null;
         segmentsOfTheField = new Segment[1];
         segmentsOfTheField[0] = segment1;
-        
+
         Map map = new Map(segmentsOfTheField, segmentWhereTheBallCanBeReleased);
-       
-        Point centerOfTheBallWanted = pointOfTheBeginnigOfTheSegment;
+
+        Point centerOfTheBallWanted = beginingOfTheSegment;
         int radiusOfTheBallWanted = 10;
         Ball ball = new Ball(radiusOfTheBallWanted, centerOfTheBallWanted);
-        
+
         String nameOfThePlayerPlaying = "toto";
-        
+
         new RollingBall(nameOfThePlayerPlaying, map, ball).play();
         System.out.print("kikou");
     }
