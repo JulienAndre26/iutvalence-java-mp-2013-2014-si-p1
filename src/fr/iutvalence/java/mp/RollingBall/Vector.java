c0e6d4658c6f4a2a28e17cdacdd7de7006ae1e7c
JulienAndre26 <julien.andre@iut-valence.fr>
2013-10-18 08:12:42 +0200

    TODOs FIXED Vector.java

--- src/fr/iutvalence/java/mp/RollingBall/Vector.java
+++ src/fr/iutvalence/java/mp/RollingBall/Vector.java
@@ -1,21 +1,23 @@
 package fr.iutvalence.java.mp.RollingBall;
 
-// TODO (fix) detail comment
+// TODO (FIXED) detail comment
 /**
- * the Vector is used to set up the physic in the game
+ * the Vector extends the Segment class and it is used to set up the physic 
+ * rules in the game
  * 
  * @author andrejul
  *
  */
 public class Vector extends Segment
 {
-    /**
-     * Vector created with the component of the vector
-     * @param componentOfTheVector 
-     */
+/**
+ *  Vector created with the component of the vector
+ * @param componentOfTheVector second point of the vector which will
+ * set up the power of the vector
+ */
     public Vector(Point componentOfTheVector)
     {
-        super(new Point(0,0), componentOfTheVector, false);
+        super(new Point(0,0), componentOfTheVector);
     }
 
 }
