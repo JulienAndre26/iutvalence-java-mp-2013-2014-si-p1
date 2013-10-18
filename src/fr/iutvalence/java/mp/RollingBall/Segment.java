ae4eefcd40fb5b7638cc9ccc36f6b45997faea8f
JulienAndre26 <julien.andre@iut-valence.fr>
2013-10-18 09:56:30 +0200

    Update Segment.java
    Created intersect method to test the intersection of a ball and a
    segment

--- src/fr/iutvalence/java/mp/RollingBall/Segment.java
+++ src/fr/iutvalence/java/mp/RollingBall/Segment.java
@@ -36,4 +36,72 @@ public class Segment
         this.startingPoint = beginningOfTheSegment;
         this.endingPoint = endingOfTheSegment;
     }
+    
+    /**
+     * method to get the ending point of the segment
+     * @return Point the ending point of the segment
+     */
+    public Point getEndingPoint()
+    {
+        return this.endingPoint;
+    }
+    
+    /**
+     * method to set up the ending point of the segment
+     * @param endingPoint the new ending point wanted
+     */
+    public void setEndingPoint(Point endingPoint)
+    {
+        this.endingPoint = endingPoint;
+    }
+    
+    
+    /**
+     * method which tests if a ball is on(/in) a segment
+     * @param currentBall the ball we test
+     * @return return a boolean, if yes : the ball is on a segment, 
+     * otherwise it's out of a segment of the map
+     */
+    public boolean intersect(Ball currentBall)
+    {
+        boolean isIntersect;
+        int a;
+        int b;
+        int c;
+        int r;
+        int x1Ball;
+        int y1Ball;
+
+        int x2Segment;
+        int y2Segment;
+
+        int x3Segment;
+        int y3Segment;
+
+        r = currentBall.getRadius();
+        x1Ball = currentBall.getMiddlePoint().getX();
+        y1Ball = currentBall.getMiddlePoint().getY();
+
+        x2Segment = this.startingPoint.getX();
+        y2Segment = this.startingPoint.getY();
+
+        x3Segment = this.endingPoint.getX();
+        y3Segment = this.endingPoint.getY();
+
+        a = (x3Segment - x2Segment)*(x3Segment - x2Segment) + (y3Segment)*(y2Segment);
+        b = 2 * ((x3Segment - x2Segment)*(x2Segment - x1Ball) + 
+                (y3Segment - y2Segment) * (y2Segment - y1Ball));
+        c = x2Segment*x2Segment + y2Segment*y2Segment + x1Ball*x1Ball + 
+                y1Ball*y1Ball - 2*(x2Segment * x1Ball + y2Segment * y1Ball) - r*r;
+        
+        if (b*b - 4*a*c < 0)
+        {
+            isIntersect = false;
+        }
+        else
+        {
+            isIntersect = true;
+        }
+        return isIntersect;
+    }
 }
