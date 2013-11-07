200ecfee1e088192fdd594a805f4b606cd332375
JulienAndre26 <julien.andre@iut-valence.fr>
2013-11-07 17:31:32 +0100

    CREATED SamePointException.java

---
+++ src/fr/iutvalence/java/mp/RollingBall/SamePointException.java
@@ -0,0 +1,12 @@
+package fr.iutvalence.java.mp.RollingBall;
+
+/**
+ * Exception raised when two points are equals
+ * 
+ * @author andrejul
+ *
+ */
+public class SamePointException extends Exception
+{
+   
+}
