578903d57aec445e63a6b95cde0fa6aa9eddd6f6
JulienAndre26 <julien.andre@iut-valence.fr>
2013-11-07 17:49:45 +0100

    UPDATED Main.java
    Updated test. it now catches exception.

--- src/fr/iutvalence/java/mp/RollingBall/Main.java
+++ src/fr/iutvalence/java/mp/RollingBall/Main.java
@@ -20,32 +20,41 @@ public class Main
         Point beginningOfTheSegment = new Point(50, 50);
         Point endingOfTheSegment = new Point(50, 100);
 
-        Segment segmentWhereTheBallCanBeReleased = new Segment(beginningOfTheSegment, endingOfTheSegment);
-        System.out.print("segment de la balle créé\n");
+        try
+        {
+            Segment segmentWhereTheBallCanBeReleased = new Segment(beginningOfTheSegment, endingOfTheSegment);
+            System.out.print("segment de la balle créé\n");
 
-        Point beginningOfTheSegment1 = new Point(0, 0);
-        Point endingOfTheSegment1 = new Point(100, 0);
+            Point beginningOfTheSegment1 = new Point(0, 0);
+            Point endingOfTheSegment1 = new Point(100, 0);
 
-        Segment segment1 = new Segment(beginningOfTheSegment1, endingOfTheSegment1);
+            Segment segment1 = new Segment(beginningOfTheSegment1, endingOfTheSegment1);
 
-        Segment[] segmentsOfTheField = null;
-        segmentsOfTheField = new Segment[1];
-        segmentsOfTheField[0] = segment1;
-        System.out.print("map créée\n");
+            Segment[] segmentsOfTheField = null;
+            segmentsOfTheField = new Segment[1];
+            segmentsOfTheField[0] = segment1;
+            System.out.print("map créée\n");
 
-        Map map = new Map(segmentsOfTheField, segmentWhereTheBallCanBeReleased);
+            Map map = new Map(segmentsOfTheField, segmentWhereTheBallCanBeReleased);
 
-        Point componentOfTheVector = new Point(0, 0);
-        Vector speedOfTheBall = new Vector(componentOfTheVector);
-        Point centerOfTheBallWanted = beginningOfTheSegment;
-        int radiusOfTheBallWanted = 10;
-        MovingBall ball = new MovingBall(radiusOfTheBallWanted, centerOfTheBallWanted, speedOfTheBall);
-        System.out.print("balle créée\n");
+            Point componentOfTheVector = new Point(0, 0);
+            Vector speedOfTheBall = new Vector(componentOfTheVector);
+            Point centerOfTheBallWanted = beginningOfTheSegment;
+            int radiusOfTheBallWanted = 10;
+            MovingBall ball = new MovingBall(radiusOfTheBallWanted, centerOfTheBallWanted, speedOfTheBall);
+            System.out.print("balle créée\n");
 
-        String nameOfThePlayerPlaying = "toto";
+            String nameOfThePlayerPlaying = "toto";
 
-        new RollingBallGame(nameOfThePlayerPlaying, map, ball).play();
-        System.out.print("le jeu a commencé\n");
+            new RollingBallGame(nameOfThePlayerPlaying, map, ball).play();
+            System.out.print("le jeu a commencé\n");
+        }
+        catch (SamePointException e)
+        {
+            e.printStackTrace();
+        }
+
+       
     }
 
 }
