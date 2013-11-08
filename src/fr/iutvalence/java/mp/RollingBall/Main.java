8de55bf858265176d59615059cf0ca282e9377ac
JulienAndre26 <julien.andre@iut-valence.fr>
2013-11-08 09:55:37 +0100

    UPDATED Main.java
    Modified test (longer than previous)

--- src/fr/iutvalence/java/mp/RollingBall/Main.java
+++ src/fr/iutvalence/java/mp/RollingBall/Main.java
@@ -17,44 +17,66 @@ public class Main
      */
     public static void main(String[] args)
     {
-        Point beginningOfTheSegment = new Point(50, 50);
-        Point endingOfTheSegment = new Point(50, 100);
-
         try
         {
-            Segment segmentWhereTheBallCanBeReleased = new Segment(beginningOfTheSegment, endingOfTheSegment);
-            System.out.print("segment de la balle créé\n");
+            Point beginningOfTheSegment = new Point(50, 20);
+            Point endingOfTheSegment = new Point(50, 50);
 
-            Point beginningOfTheSegment1 = new Point(0, 0);
-            Point endingOfTheSegment1 = new Point(100, 0);
+            Segment segmentWhereTheBallCanBeReleased = new Segment(beginningOfTheSegment, endingOfTheSegment);
+            System.out.println("segment de la balle créé");
+
+            Point beginningOfTheSegment1 = new Point(0, 10);
+            Point endingOfTheSegment1 = new Point(100, -10);
 
             Segment segment1 = new Segment(beginningOfTheSegment1, endingOfTheSegment1);
+            
+            Point beginningOfTheSegment2 = new Point(100, -10);
+            Point endingOfTheSegment2 = new Point(150, 30);
+
+            Segment segment2 = new Segment(beginningOfTheSegment2, endingOfTheSegment2);
+            
+            Point beginningOfTheSegment3 = new Point(0, 10);
+            Point endingOfTheSegment3 = new Point(0, 100);
+
+            Segment segment3 = new Segment(beginningOfTheSegment3, endingOfTheSegment3);
+            
+            Point beginningOfTheSegment4 = new Point(150, 30);
+            Point endingOfTheSegment4 = new Point(150, 100);
+
+            Segment segment4 = new Segment(beginningOfTheSegment4, endingOfTheSegment4);
+            
+            Point beginningOfTheSegment5 = new Point(0, 100);
+            Point endingOfTheSegment5 = new Point(150, 100);
+
+            Segment segment5 = new Segment(beginningOfTheSegment5, endingOfTheSegment5);
 
             Segment[] segmentsOfTheField = null;
-            segmentsOfTheField = new Segment[1];
+            segmentsOfTheField = new Segment[5];
             segmentsOfTheField[0] = segment1;
-            System.out.print("map créée\n");
+            segmentsOfTheField[1] = segment2;
+            segmentsOfTheField[2] = segment3;
+            segmentsOfTheField[3] = segment4;
+            segmentsOfTheField[4] = segment5;
+            System.out.println("map créée");
 
             Map map = new Map(segmentsOfTheField, segmentWhereTheBallCanBeReleased);
 
-            Point componentOfTheVector = new Point(0, 0);
-            Vector speedOfTheBall = new Vector(componentOfTheVector);
+            Vector speedOfTheBall = new Vector(0, 0);
             Point centerOfTheBallWanted = beginningOfTheSegment;
             int radiusOfTheBallWanted = 10;
             MovingBall ball = new MovingBall(radiusOfTheBallWanted, centerOfTheBallWanted, speedOfTheBall);
-            System.out.print("balle créée\n");
+            System.out.println("balle créée");
 
             String nameOfThePlayerPlaying = "toto";
 
-            new RollingBallGame(nameOfThePlayerPlaying, map, ball).play();
-            System.out.print("le jeu a commencé\n");
+            RollingBallGame theGame = new RollingBallGame(nameOfThePlayerPlaying, map, ball);
+            System.out.println("le jeu commence !!");
+            theGame.play();
         }
         catch (SamePointException e)
         {
             e.printStackTrace();
-        }
-
-       
+        }       
     }
 
 }
