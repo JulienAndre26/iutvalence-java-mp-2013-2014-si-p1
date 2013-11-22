8c23dd2f728f6c02b3ed5f7bac8c6ca2ea287cde
JulienAndre26 <julien.andre@iut-valence.fr>
2013-11-22 09:54:55 +0100

    UPDATED Player.java
    Changed class Display used instead of using system.out.println

--- src/fr/iutvalence/java/mp/RollingBall/Player.java
+++ src/fr/iutvalence/java/mp/RollingBall/Player.java
@@ -21,29 +21,29 @@ public class Player
 
         Point beginningOfTheSegment1 = new Point(0, 10);
         Point endingOfTheSegment1 = new Point(100, -10);
-        
+
         Point beginningOfTheSegment2 = new Point(100, -10);
         Point endingOfTheSegment2 = new Point(150, 30);
-        
+
         Point beginningOfTheSegment3 = new Point(0, 10);
         Point endingOfTheSegment3 = new Point(0, 100);
-        
+
         Point beginningOfTheSegment4 = new Point(150, 30);
         Point endingOfTheSegment4 = new Point(150, 100);
-        
+
         Point beginningOfTheSegment5 = new Point(0, 100);
         Point endingOfTheSegment5 = new Point(150, 100);
-        
+
         Vector speedOfTheBall = new Vector(0, 0);
         Point centerOfTheBallWanted = beginningOfTheSegment;
         int radiusOfTheBallWanted = 10;
-        
+
         Segment segmentWhereTheBallCanBeReleased;
-        
+
         try
         {
             segmentWhereTheBallCanBeReleased = new Segment(beginningOfTheSegment, endingOfTheSegment);
-            
+
             Segment segment1 = new Segment(beginningOfTheSegment1, endingOfTheSegment1);
 
             Segment segment2 = new Segment(beginningOfTheSegment2, endingOfTheSegment2);
@@ -53,7 +53,7 @@ public class Player
             Segment segment4 = new Segment(beginningOfTheSegment4, endingOfTheSegment4);
 
             Segment segment5 = new Segment(beginningOfTheSegment5, endingOfTheSegment5);
-            
+
             Segment[] segmentsOfTheField = null;
             segmentsOfTheField = new Segment[5];
             segmentsOfTheField[0] = segment1;
@@ -61,17 +61,17 @@ public class Player
             segmentsOfTheField[2] = segment3;
             segmentsOfTheField[3] = segment4;
             segmentsOfTheField[4] = segment5;
-            System.out.println("map créée");
-            
+            Display.mapCreated();
+
             Map map = new Map(segmentsOfTheField, segmentWhereTheBallCanBeReleased);
-            
+
             MovingBall ball = new MovingBall(radiusOfTheBallWanted, centerOfTheBallWanted, speedOfTheBall);
-            System.out.println("balle créée");
-            
+            Display.ballCreated();
+
             String nameOfThePlayerPlaying = "toto";
-            
+
             RollingBallGame theGame = new RollingBallGame(nameOfThePlayerPlaying, map, ball);
-            
+
             return theGame;
         }
         catch (NullVectorException e)
