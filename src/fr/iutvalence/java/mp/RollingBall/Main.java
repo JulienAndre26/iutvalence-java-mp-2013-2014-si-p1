ca6dac87277b3f0a61c5441a420afcdde2b6cd9b
Clémence <Clmence@192.168.1.12>
2013-12-22 16:14:37 +0100

    UPDATED Main.java
    Changed map

--- src/fr/iutvalence/java/mp/RollingBall/Main.java
+++ src/fr/iutvalence/java/mp/RollingBall/Main.java
@@ -1,47 +1,58 @@
 package fr.iutvalence.java.mp.RollingBall;
 
-// TODO: Auto-generated Javadoc
+
 /**
- * main class of the rollingball game.
+ *
+ * main class of the rollingball game
  *
  * @author andrejul
+ *
  */
 public class Main
 {
-
     /**
-     * start a play of the rollingball game.
+     * start a play of the rollingball game
      *
-     * @param args undefined reference
+     * @param args
+     * undefined reference
      */
     public static void main(String[] args)
     {
-        //	ConsoleDisplay display = new ConsoleDisplay();
+        //  ConsoleDisplay display = new ConsoleDisplay();
         GraphicalDisplay display = new GraphicalDisplay();
         PlayerFixed player = new PlayerFixed();
 
-        Point beginningOfTheSegment = new Point(50, 50);
-        Point endingOfTheSegment = new Point(50, 75);
+        Point beginningOfTheSegment = new Point(10, 100);
+        Point endingOfTheSegment = new Point(10, 90);
 
-        Point beginningOfTheSegment1 = new Point(0, 10);
-        Point endingOfTheSegment1 = new Point(100, -10);
+        Point beginningOfTheSegment1 = new Point(5, 80);
+        Point endingOfTheSegment1 = new Point(30, 75);
 
-        Point beginningOfTheSegment2 = new Point(100, -10);
-        Point endingOfTheSegment2 = new Point(150, 30);
+        Point beginningOfTheSegment2 = new Point(30, 65);
+        Point endingOfTheSegment2 = new Point(40, 70);
 
-        Point beginningOfTheSegment3 = new Point(0, 10);
-        Point endingOfTheSegment3 = new Point(0, 100);
+        Point beginningOfTheSegment3 = new Point(40, 70);
+        Point endingOfTheSegment3 = new Point(65, 65);
 
-        Point beginningOfTheSegment4 = new Point(150, 30);
-        Point endingOfTheSegment4 = new Point(150, 100);
+        Point beginningOfTheSegment4 = new Point(70, 60);
+        Point endingOfTheSegment4 = new Point(90, 40);
 
-        Point beginningOfTheSegment5 = new Point(0, 100);
-        Point endingOfTheSegment5 = new Point(150, 100);
+        Point beginningOfTheSegment5 = new Point(90, 40);
+        Point endingOfTheSegment5 = new Point(130, 35);
+
+        Point beginningOfTheSegment6 = new Point(135,30);
+        Point endingOfTheSegment6 = new Point(135,10);
+
+        Point beginningOfTheSegment7 = new Point(136, 10);
+        Point endingOfTheSegment7 = new Point(300, 10);
+
+        Point beginningOfTheSegment8 = new Point(300, 10);
+        Point endingOfTheSegment8 = new Point(300, 300);
 
         Vector speedOfTheBall = new Vector(0, 0);
         Point centerOfTheBallWanted = new Point(0,0);
-        int radiusOfTheBallWanted = 15;
-        //int radiusOfTheBallWanted = 10;
+        //int radiusOfTheBallWanted = 15;
+        int radiusOfTheBallWanted = 10;
 
         try
         {
@@ -57,19 +68,27 @@ public class Main
 
             Segment segment5 = new Segment(beginningOfTheSegment5, endingOfTheSegment5);
 
+            Segment segment6 = new Segment(beginningOfTheSegment6, endingOfTheSegment6);
+
+            Segment segment7 = new Segment(beginningOfTheSegment7, endingOfTheSegment7);
+
+            Segment segment8 = new Segment(beginningOfTheSegment8, endingOfTheSegment8);
+
             Segment[] segmentsOfTheField = null;
-            segmentsOfTheField = new Segment[5];
+            segmentsOfTheField = new Segment[8];
             segmentsOfTheField[0] = segment1;
             segmentsOfTheField[1] = segment2;
             segmentsOfTheField[2] = segment3;
             segmentsOfTheField[3] = segment4;
             segmentsOfTheField[4] = segment5;
+            segmentsOfTheField[5] = segment6;
+            segmentsOfTheField[6] = segment7;
+            segmentsOfTheField[7] = segment8;
+
             display.mapCreated();
             Map map = new Map(segmentsOfTheField, segmentWhereTheBallCanBeReleased);
 
             String nameOfThePlayerPlaying = "toto";
-
-            centerOfTheBallWanted = display.getStartingPoint(segmentWhereTheBallCanBeReleased);
 
             MovingBall ball = new MovingBall(radiusOfTheBallWanted, centerOfTheBallWanted, speedOfTheBall);
 
